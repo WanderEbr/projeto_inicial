a577b65ce2e1810dfda7ad3a6f57ee04368cdf4f
wanderedbr <wbrukoski@gmail.com>
2024-09-23 19:47:37 -0300

    meu primeiro commit

---
+++ python/main.py
@@ -0,0 +1,19 @@
+print ("Jornada de dados")
+
+# variaveis
+idade=46
+nome="wander"
+
+# print
+print(idade)
+
+# soma
+nova_idade=idade+1
+print(nova_idade)
+
+# lista
+lista=[1,2,3,4,5]
+
+# metodos
+lista_invertida=lista.reverse()
+print(lista_invertida)
